bf3383f3956c6afded3cc756ea85844eac487459
Иван Небеддаг <nebeddag@gamil.com>
2020-09-20 22:18:13 +0300

    gragient function generalized

--- tests.py
+++ tests.py
@@ -1,23 +1,26 @@
-#%%
-import gradients as gr
+
+# %%
+from gradient import Gradient
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-# %%
-g = gr.GreenYellowRedGradient(0, 50, 100)
+stgs = []
+stgs.append((100, (0,0,0)))
+stgs.append((80, (255,0,255)))
+stgs.append((0, (0,0,255)))
+stgs.append((60, (0,255,255)))
+stgs.append((20, (0,255,0)))
+stgs.append((40, (255,255,0)))
+
+gr = Gradient(stgs)
 im = np.zeros((100,100,3)).astype(int)
 
 for i in range(100):
-    rgb = g.get_rgb(i)
+    rgb = gr.get_color(i)
     im[:,i,0] = rgb[0]
     im[:,i,1] = rgb[1]
     im[:,i,2] = rgb[2]
-# %%
+    
 plt.imshow(im)
-# %%
-import cv2
-cv2.imshow('1', im)
-# %%
-
 # %%
