d288e9648dd8c224df6c3f7c3db4d0a2ff7a3a99
Ivan Klimenko <ivan.v.klimenko@gmail.com>
2020-12-08 14:24:00 +0300

    add names randomizing to dataset splitting

--- Dataset/split_dataset.py
+++ Dataset/split_dataset.py
@@ -1,26 +1,38 @@
 import random
 import glob, os
 import errno
+import uuid
 
 init_dir = '/media/cundo/HardDisk_01/Datasets/cleaning_data/ForReport/train'
 export_dir = '/media/cundo/HardDisk_01/Datasets/cleaning_data/ForReport/test'
 
 export_fraction = 0.2
+randomize_names = True
 
 for (dirpath, dirnames, filenames) in os.walk(init_dir):
-
-    files = [dirpath + '/' + fn for fn in filenames]
     newdir = dirpath.replace(init_dir,export_dir)
-
-    if len(files) == 0:
-        continue
 
     if not os.path.exists(newdir):
         os.makedirs(newdir)
-    fs_cnt = int(len(files) * export_fraction)
-    moving_files = random.sample(files, fs_cnt)
+
+    if len(filenames) == 0:
+        continue
+
+    if randomize_names:
+        filenames_randomized = []
+        for f in filenames:
+            nf = f"{uuid.uuid4().hex}.{f.split('.')[-1]}"
+            filenames_randomized.append(nf)
+            f_old = os.path.join(dirpath, f)
+            f_new = f_old.replace(f, nf)
+            os.rename(f_old, f_new)
+        filenames = filenames_randomized
+
+    fs_cnt = int(len(filenames) * export_fraction)
+    moving_files = random.sample(filenames, fs_cnt)
     for f in moving_files:
-        f_new = f.replace(init_dir, export_dir)
-        os.rename(f, f_new)
+        f_old = os.path.join(dirpath, f)
+        f_new = f_old.replace(init_dir, export_dir)
+        os.rename(f_old, f_new)
     
 
