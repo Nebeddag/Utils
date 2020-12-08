22e44919d67b60803d7add450cf66769246b360c
Ivan Klimenko <ivan.v.klimenko@gmail.com>
2020-12-08 13:53:06 +0300

    resizing with padding added

--- crop.py
+++ crop.py
@@ -13,22 +13,23 @@ def make_square(img: np.array, new_h: int = -1):
     f[ay:img.shape[0]+ay,ax:ax+img.shape[1]] = img
     return f
 
-#resizing image for model training or inference
-def soft_resize(img: np.array, size):
-    old_ratio = img.shape[0]/img.shape[1]
-    new_ratio = size[0]/size[1]
+def resize_with_pad(self, img: np.array, new_size):
+    old_ratio = img.shape[1]/img.shape[0]
+    new_ratio = new_size[0]/new_size[1]
 
-    if(new_ratio > old_ratio):
-        new_w = size[0]
-        new_h = new_w / old_ratio
+    if(new_ratio < old_ratio):
+        new_h = new_size[0]
+        new_w = int(new_h / old_ratio)
     else:
-        new_h = size[1]
-        new_w = new_h * old_ratio
+        new_w = new_size[1]
+        new_h = int(new_w * old_ratio)
 
-    img = cv2.resize(img, new_w, new_h)
+    img = cv2.resize(img, (new_h, new_w))
 
-    f = np.zeros((size[0],size[1],3),np.uint8)
-    f[0:img.shape[0],0:img.shape[1]] = img
+    f = np.zeros((new_size[1],new_size[0],3),np.uint8)
+    shift_x = int((new_size[1] - img.shape[0])/2)
+    shift_y = int((new_size[0] - img.shape[1])/2)
+    f[shift_x:img.shape[0]+shift_x,shift_y:img.shape[1]+shift_y] = img
     return f
 
 def change_ratio_soft(img: np.array, new_ratio, shift = 0):
